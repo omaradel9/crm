24806c18c6411acb810629a061aa03fa74f9cec2
basant <basantgaber991@gmail.com>
2024-03-18 09:56:50 +0200

    move salesperson and seles team

---
+++ hide_fields/__manifest__.py
@@ -0,0 +1,38 @@
+{
+    "name": "Hide fields",
+    "description": "sale module",
+    "author": "Basant Gaber Abd-Elaziz",
+    "data": [
+        'views/sale_order_view.xml',
+    
+      
+     
+
+       
+      
+     
+        
+   
+        
+       
+
+    ],
+    'sequence':'-1',
+    "depends": ["base","sale","sale_order_reports","modify_modules"],
+
+    'installable': True,
+    'application': True,
+
+
+
+
+
+    
+   
+
+   
+    
+    
+  
+  
+}

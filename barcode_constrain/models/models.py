794c4a2d97ac27afcb66c0e4a3b3e822ff3c9054
basant <basantgaber991@gmail.com>
2024-03-21 16:52:37 +0200

    add terms conditions 21/3

--- barcode_constrain/models/models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class ProductProduct(models.Model):
-    _inherit = "product.template"
-    alt_barcode = fields.Char(
-        'Barcode', copy=False, index='btree_not_null',
-        help="International Article Number used for product identification.")

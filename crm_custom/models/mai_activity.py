5c08159947858ba8f6b471c06ce1d49a3c23abb3
basant <basantgaber991@gmail.com>
2024-06-25 14:02:07 +0300

    create the activity for the opened record lead else for the default lead

--- crm_custom/models/mai_activity.py
+++ crm_custom/models/mai_activity.py
@@ -19,11 +19,11 @@ class MailActivity(models.Model):
 
     @api.model
     def default_get(self, fields):
+        
         res = super(MailActivity, self).default_get(fields)
-
         lead_id = self.env['ir.config_parameter'].sudo().get_param('crm_custom.crm_lead_id')
 
-        if lead_id:
+        if 'default_res_model' not in self.env.context and lead_id :
             res['lead_id'] = int(lead_id)
             res['res_model_id'] = self.env['ir.model']._get_id('crm.lead')
             res['res_id'] = res['lead_id']   
